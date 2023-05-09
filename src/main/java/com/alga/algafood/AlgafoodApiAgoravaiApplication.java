ada8190258031df098e4f1b9462134bd67814dd8
eder15720 <henri.eder@outlook.com>
2023-05-09 00:52:07 -0300

    inserção do projeto

---
+++ src/main/java/com/alga/algafood/AlgafoodApiAgoravaiApplication.java
@@ -0,0 +1,13 @@
+package com.alga.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiAgoravaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiAgoravaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alga/algafood/AlgafoodApiAgoravaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alga.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiAgoravaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
